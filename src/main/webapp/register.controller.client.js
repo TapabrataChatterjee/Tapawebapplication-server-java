3b2bb0107332421bea902522d333e4a503e888c2
TapabrataChatterjee <chatterjee.tapa@gmail.com>
2019-11-21 16:36:31 -0500

    added login controller for client

---
+++ src/main/webapp/register.controller.client.js
@@ -0,0 +1,30 @@
+(function () {
+    var registerBtn = jQuery('#registerBtn');
+    registerBtn.click(function () {
+        var username = $('#username');
+        var password = jQuery('#password');
+        var password2 = jQuery('#password2');
+        if (password.val() == password2.val()) {
+            document.getElementById("password2").insertAdjacentText("afterend", "Passwords match");
+            var passwordobj = password.val();
+            var usernameobj = username.val();
+
+            var userobj = {
+                username: usernameobj,
+                password: passwordobj
+            }
+            console.log(userobj);
+
+
+        }
+        else {
+            //  alert('passwords dont match');
+            document.getElementById("password2").value = "";
+            document.getElementById("password2").insertAdjacentText("afterend", "Passwords dont match");
+        }
+
+
+
+    })
+})();
+
