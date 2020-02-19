6ebeb732bedf16f32b377bd9723df0d8284798c5
TapabrataChatterjee <chatterjee.tapa@gmail.com>
2020-02-18 23:34:08 -0500

    changes to register controller

--- src/main/webapp/register.controller.client.js
+++ src/main/webapp/register.controller.client.js
@@ -1,11 +1,13 @@
 (function () {
     var registerBtn = jQuery('#registerBtn');
     registerBtn.click(function () {
+        alert("Register button clicked");
         var username = $('#username');
+        username.css('color','red');
         var password = jQuery('#password');
         var password2 = jQuery('#password2');
         if (password.val() == password2.val()) {
-            document.getElementById("password2").insertAdjacentText("afterend", "Passwords match");
+        //    document.getElementById("password2").insertAdjacentText("afterend", "Passwords match");
             var passwordobj = password.val();
             var usernameobj = username.val();
 
@@ -19,10 +21,11 @@
         }
         else {
             //  alert('passwords dont match');
-            document.getElementById("password2").value = "";
-            document.getElementById("password2").insertAdjacentText("afterend", "Passwords dont match");
+          //  document.getElementById("password2").value = "";
+          //  document.getElementById("password2").insertAdjacentText("afterend", "Passwords dont match");
+            password2.insertAdjacentText("Passwords donot match, Please re-enter");
         }
-
+        
 
 
     })
